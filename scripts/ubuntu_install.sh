8be362b1ba5fffe96b3df9d9311f579b51f66dce
Mark Veltzer <mark.veltzer@gmail.com>
2014-06-29 15:06:39 +0300

    better install script

--- scripts/ubuntu_install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# this scrip will install all the required packages that you need on
-# ubuntu to compile and work with this package.
-
-sudo apt-get install lacheck latex2html sketch texlive-latex-base texlive-binaries ghostscript qpdf sketch-doc poppler-utils luatex pgf tex4ht
