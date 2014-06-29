c12315d2e8d82e5bcf02463c816be06ccd8ee4ba
Mark Veltzer <mark@veltzer.net>
2014-06-29 15:06:39 +0300

    better install script

---
+++ scripts/ubuntu_install.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# this script will install all the required packages that you need on
+# ubuntu to compile and work with this package.
+
+# TODO:
+
+import subprocess # for check_call()
+
+packs=[
+	'lacheck',
+	'latex2html',
+	'sketch',
+	'texlive-latex-base',
+	'texlive-binaries',
+	'ghostscript',
+	'qpdf',
+	'sketch-doc',
+	'poppler-utils',
+	'luatex',
+	'pgf',
+	'tex4ht',
+]
+
+args=['sudo','apt-get','install']
+args.extend(packs)
+subprocess.check_call(args)
