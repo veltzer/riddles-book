fc9f6edf90cb48d00a84a6e38a443bc262c402b4
Mark Veltzer <mark.veltzer@gmail.com>
2014-11-17 09:44:35 +0200

    better README.md

--- templardefs/project.py
+++ templardefs/project.py
@@ -20,23 +20,19 @@ def populate(d):
 	]
 	d.project_license='GPLV3'
 	d.project_year_started='2011'
-	d.project_description='''This is the riddling project
-
-The idea is to create a collection of many riddles for your enjoyment.
+	d.project_description='''The idea is to create a collection of many riddles for your enjoyment.
 
 Tools include: make, pdflatex, lacheck, latex, dvips, ps2pdf, latex2html, perl, sketch, git,
         flexpaper, pdf2swf, qpdf, pdfinfo, pdftex, pdftohtml, luatex, xetex and possibly more.
 
 If you want to see the result checkout:
-https://veltzer.net/blog/riddling/
+{project_website}
 
 If you want to compile this package then:
-- install all required packages.
+* install all required packages.
         Ubuntu users you are in luck: just run 'scripts/ubuntu_install.sh'...
-- make
+* make
 All the output is in the 'out' folder.
-
-        Mark Veltzer, 2011-2014
 '''.format(**d)
 
 	# deb
