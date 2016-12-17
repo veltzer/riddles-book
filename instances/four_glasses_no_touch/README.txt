bbd9e00819d6e9ceccf8eed445cbc83ffc78a060
Mark Veltzer <mark.veltzer@gmail.com>
2016-12-18 01:09:44 +0200

    this riddle is done

---
+++ instances/four_glasses_no_touch/README.txt
@@ -0,0 +1,15 @@
+Four glasses are pladced on the corners of a square rotating table.
+Some of the glasses are facing upwards and some upside-down.
+
+Your goal is to arrange the glasses so that they are all facing up or all facing down. Here are the rules:
+
+- You must keep your eyes closed at all times. (No tricks or lateral thinking, this is a pure logic puzzle)
+
+- You can turn change the state of (flip) any number of glasses at any turn (turning all 4 is useless but 1
+  2 or 3 can be done).
+
+- After each turn, the table is rotated through a random angle (0, 90, 180, 270)
+
+- At any point, if all four glasses are of the same orientation a bell will ring and you win.
+
+Find a solution to ensure that all glasses have the same orientation (either up or down) in a finite number of turns. The algorithm must not depend on luck.
---
+++ instances/four_glasses_no_touch/test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+"""
+This program tests the solution for the riddle
+"""
+
+import random
+
+
+options = [True, False]
+
+def generate_state_state():
+    return (
+            random.choice(options),
+            random.choice(options),
+            random.choice(options),
+            random.choice(options),
+    )
+
+def get_turn_angle():
+    return random.choice([0,1,2,3])
+
+def apply_turn(state, turn):
+    return state[turn % 4], state[(1+turn) % 4], state[(2+turn) % 4], state[(3+turn) % 4]
+
+def apply_mod(state, mod):
+    if mod=="c":
+        return not state[0], state[1], not state[2], state[3]
+    if mod=="b":
+        return not state[0], not state[1], state[2], state[3]
+    if mod=="a":
+        return not state[0], state[1], state[2], state[3]
+
+def check_win(state):
+    return len(set(state))==1
+
+experiments = 1000000
+for _ in range(experiments):
+    state = generate_state_state()
+    if check_win(state):
+        continue
+    for mod in ["c", "b", "c", "a", "c", "b", "c"]:
+        state = apply_mod(state, mod)
+        if check_win(state):
+            break
+        state = apply_turn(state, get_turn_angle())
+    else:
+        raise ValueError("we didn't win?!?")
