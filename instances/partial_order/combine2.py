6c688c6bb2bbe87e4ff3a64f7e7468d127ed0205
Mark Veltzer <mark.veltzer@gmail.com>
2017-06-08 17:40:07 +0300

    another solution for this exercise

---
+++ instances/partial_order/combine2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def gen(*data):
+   yield from data
+
+
+def append_next_or_drop(a, aa, bb):
+   try:
+       aa.append(next(a))  # either append one new item to aa...
+   except StopIteration:
+       a.close()  # This may be called more than once --- but this is ok.
+       bb.pop(0)  # or pop of the first element of bb.
+
+
+def combine(a, b):
+   aa = []
+   bb = []
+   while True:
+       append_next_or_drop(a, aa, bb)
+       append_next_or_drop(b, bb, aa)
+       if len(aa) == 0:
+           break
+       yield list(zip(aa, reversed(bb)))
+
+
+for data in combine(gen(1,2,3,4), gen(1,2,3,4)):
+   print(data)
