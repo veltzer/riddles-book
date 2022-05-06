56b763a99fafcf2ccbffc56ab85c09f901840ae1
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:47 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,10 +2,6 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = []
-setup_requires = []
-run_requires = []
-test_requires = []
 dev_requires = [
     "pymakehelper",
     "pydmt",
@@ -13,10 +9,6 @@ dev_requires = [
     "numpy",
     "BitVector",
 ]
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
