fd4519d3c2a1961c4424b14c211c24e8e363e148
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 14:28:19 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -10,6 +10,7 @@ dev_requires = [
     "BitVector",
 ]
 
-python_requires = ">=3.9"
-test_os = ["ubuntu-20.04"]
-test_python = ["3.9"]
+python_requires = ">=3.10"
+
+test_os = ["ubuntu-22.04"]
+test_python = ["3.10"]
