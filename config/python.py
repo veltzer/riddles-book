a99c7810e07aa3d24c913f9dd05a5c3ded35fb66
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 03:57:49 +0200

    no commit message given

--- config/python.py
+++ config/python.py
@@ -1,10 +1,17 @@
-make_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = []
+install_requires: List[str] = [
+    "numpy",
+    "BitVector",
+]
+make_requires: List[str] = [
     "pymakehelper",
     "pyclassifiers",
     "pydmt",
     "pylint",
 ]
-install_requires = [
-    "numpy",
-    "BitVector",
-]
+test_requires: List[str] = []
+requires = config_requires + install_requires + make_requires + test_requires
