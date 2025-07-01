7cae87e3abaf19598dda14487f66a4fdaaa1bb24
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:18 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,15 +1,12 @@
 """ python deps for this project """
 
+import config.shared
+
 install_requires: list[str] = [
     "numpy",
     "BitVector",
     "tqdm",
 ]
-build_requires: list[str] = [
-    "pydmt",
-    "pymakehelper",
-]
-test_requires: list[str] = [
-    "pylint",
-]
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
 requires = install_requires + build_requires + test_requires
